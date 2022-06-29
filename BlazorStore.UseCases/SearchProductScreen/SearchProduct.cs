5d852531c92f66feede412a2bb07c1d28e2b5abb
Vinicius Martins <viniciusmv91@gmail.com>
2022-06-29 00:08:02 -0300

    Implementing Use Cases Layer

---
+++ BlazorStore.UseCases/SearchProductScreen/SearchProduct.cs
@@ -0,0 +1,15 @@
+﻿using BlazorStore.CoreBusiness.Models;
+using System;
+using System.Collections.Generic;
+
+namespace BlazorStore.UseCases.SearchProductScreen
+{
+    public class SearchProduct
+    {
+        public IEnumerable<Product> Execute(string filter)
+        {
+            return null;
+        }
+
+    }
+}
---
+++ BlazorStore.UseCases/ViewProductScreen/ViewProduct.cs
@@ -0,0 +1,13 @@
+﻿using BlazorStore.CoreBusiness.Models;
+using System;
+
+namespace BlazorStore.UseCases.ViewProductsScreen
+{
+    public class ViewProduct
+    {
+        public Product Execute(string filter)
+        {
+            return null;
+        }
+    }
+}
