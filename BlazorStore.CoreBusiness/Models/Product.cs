f8f8b9adaff7baaf25acfc6d26c6ec879c165e0d
Vinicius Martins <viniciusmv91@gmail.com>
2022-06-29 00:03:25 -0300

    Implementing Core Business Layer

---
+++ BlazorStore.CoreBusiness/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlazorStore.CoreBusiness.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string ImageLink { get; set; }
+    }
+}
