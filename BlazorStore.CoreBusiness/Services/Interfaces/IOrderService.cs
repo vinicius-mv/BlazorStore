93b8e8062cffe3ed4a9cf1c335f7da92064c451e
Vinicius Martins <viniciusmv91@gmail.com>
2022-07-03 23:25:59 -0300

    Order Entity and Business Rules

---
+++ BlazorStore.CoreBusiness/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using BlazorStore.CoreBusiness.Models;
+
+namespace BlazorStore.CoreBusiness.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        bool ValidateCreateOrder(Order order);
+        bool ValidateCustomerInformation(string name, string address, string city, string province, string country);
+        bool ValidateProcessOrder(Order order);
+        bool ValidateUpdateOrder(Order order);
+    }
+}
