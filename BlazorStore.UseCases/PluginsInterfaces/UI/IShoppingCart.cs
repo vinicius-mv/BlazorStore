540b1c2d6145f49610c16214565e0de295ac2edc
Vinicius Martins <viniciusmv91@gmail.com>
2022-07-04 23:33:01 -0300

    Shopping Cart Abstraction

---
+++ BlazorStore.UseCases/PluginsInterfaces/UI/IShoppingCart.cs
@@ -0,0 +1,25 @@
+﻿using BlazorStore.CoreBusiness.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorStore.UseCases.PluginsInterfaces.UI
+{
+    internal interface IShoppingCart
+    {
+        Task<Order> GetOrderAsync();
+
+        Task<Order> AddProductÁsync(Product product);
+
+        Task<Order> UpdateQuantityAsync(int productId, int quantity);
+
+        Task<Order> UpdateOrderAsync(Order order);
+
+        Task<Order> DeleteProductAsync(int productId);
+
+        Task<Order> PlaceOrderAsync();
+
+        Task EmptyAsync();
+    }
+}
