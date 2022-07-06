4191fd658255a77bf934cb8a39262df8acb2e38f
Vinicius Martins <viniciusmv91@gmail.com>
2022-07-06 01:19:26 -0300

    View Shopping Cart Use Case

---
+++ BlazorStore.UseCases/ShoppingCartScreen/ViewShoppingCart.cs
@@ -0,0 +1,24 @@
+﻿using BlazorStore.CoreBusiness.Models;
+using BlazorStore.UseCases.PluginsInterfaces.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorStore.UseCases.ShoppingCartScreen
+{
+    public class ViewShoppingCartUseCase
+    {
+        private readonly IShoppingCart shoppingCart;
+
+        public ViewShoppingCartUseCase(IShoppingCart shoppingCart)
+        {
+            this.shoppingCart = shoppingCart;
+        }
+
+        public Task<Order> Execute()
+        {
+            return shoppingCart.GetOrderAsync();
+        }
+    }
+}
