a5887e0bb60e35b2e6256da7d9904d24b1fb3353
Vinicius Martins <viniciusmv91@gmail.com>
2022-07-07 01:03:20 -0300

    Order Repository

---
+++ Plugins/BlazorStore.DataStore.HardCoded/OrderRepository.cs
@@ -0,0 +1,74 @@
+﻿using BlazorStore.CoreBusiness.Models;
+using BlazorStore.UseCases.PluginsInterfaces.DataStore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlazorStore.DataStore.HardCoded
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private Dictionary<int, Order> orders;
+
+        public OrderRepository()
+        {
+            orders = new Dictionary<int, Order>();
+        }
+
+        public int CreateOrder(Order order)
+        {
+            order.OrderId = this.orders.Count + 1;
+            order.UniqueId = Guid.NewGuid().ToString();
+            this.orders.Add(order.OrderId.Value, order);
+
+            return order.OrderId.Value;
+        }
+        public IEnumerable<Order> GetOrders()
+        {
+            return orders.Values;
+        }
+
+        public IEnumerable<Order> GetOutstandingOrders()
+        {
+            var allOrders = (IEnumerable<Order>)orders.Values;
+
+            return allOrders.Where(x => x.DateProcessed.HasValue == false);
+        }
+
+        public IEnumerable<Order> GetProcessedOrders()
+        {
+            var allOrders = (IEnumerable<Order>)orders.Values;
+
+            return allOrders.Where(x => x.DateProcessed.HasValue);
+        }
+
+        public Order GetOrder(int id)
+        {
+            return orders[id];
+        }
+
+        public Order GetOrderByUniqueId(string uniqueId)
+        {
+            foreach (var order in this.orders)
+                if (order.Value.UniqueId == uniqueId)
+                    return order.Value;
+
+            return null;
+        }
+        public void UpdateOrder(Order order)
+        {
+            if (order == null || order.OrderId.HasValue == false) return;
+
+            var ord = this.orders[order.OrderId.Value];
+            if (ord == null) return;
+
+            orders[order.OrderId.Value] = order;
+        }
+
+        public IEnumerable<OrderLineItem> GetLineItemsByOrderId(int orderId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
