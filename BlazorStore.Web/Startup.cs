78d1ef675351baf64677a80555f90e8cf36566b8
Vinicius Martins <viniciusmv91@gmail.com>
2022-07-07 00:36:04 -0300

    Delete and Update with Order Line Item Component

--- BlazorStore.Web/Startup.cs
+++ BlazorStore.Web/Startup.cs
@@ -47,6 +47,8 @@ namespace BlazorStore.Web
             services.AddTransient<IViewProductUseCase, ViewProductUseCase>();
             services.AddTransient<IAddProductToCartUseCase, AddProductToCartUseCase>();
             services.AddTransient<IViewShoppingCartUseCase, ViewShoppingCartUseCase>();
+            services.AddTransient<IDeleteProductUseCase, DeleteProductUseCase>();
+            services.AddTransient<IUpdateQuantityUseCase, UpdateQuantityUseCase>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
